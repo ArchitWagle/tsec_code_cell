e11466eb4531a1cbc4ac61002403332203276ab0
Madhav Wagle <architwagle@gmail.com>
2018-07-08 01:13:32 +0300

    Update solution1_2.py

--- solution1_2.py
+++ solution1_2.py
@@ -1,14 +1,16 @@
 
-
 def sally(s):
     k=[]
     count = 0
-    k.append(s[0])
-    for i in s[1:]:
-        if(k[-1]==i):
+    #k.append(s[0])
+    for i in s:
+        print (i,k)
+        if(len(k)==0):
+            k.append(i)
+        elif(k[-1]==i):
             count = count+1
             k.pop()
         else:
             k.append(i)
-    return (count)        
-    
+#        print(i,k)
+    return (count)     
