39ad23d5177a2f78e6dfd9b572c2a9f4ea067dd6
Madhav Wagle <architwagle@gmail.com>
2018-07-08 00:24:29 +0300

    Create solution1_1.py

---
+++ solution1_1.py
@@ -0,0 +1,12 @@
+S = "Just finished the TSEC CodeCell Challenge".replace(" ","").lower()
+C = "CodeCell".lower()
+
+def decode(S,C):
+    C = C*(len(S)//len(C)+1)
+    Decode = []
+    print(C)
+    print(S)
+    for i in range(len(S)):
+        Decode.append(chr(ord('a')+(ord(S[i])+ord(C[i]))%26))
+    print("".join(Decode))
+decode(S,C)
