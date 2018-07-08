7222b91ff903350c85d52da06ce4d8a76a5501aa
Madhav Wagle <architwagle@gmail.com>
2018-07-08 20:00:43 +0300

    Update solution2_2.py

--- solution2_2.py
+++ solution2_2.py
@@ -1,9 +1,4 @@
 from math import *
-
 def ln2(x):
     return(pow(2,floor(log(x,2))))
-
-
 print((ln2(10)+ln2(100)+ln2(1000000)+ln2(1000000000)+ln2(12345654321)+ln2(9999999999999999 ))%1000000007)
-
-
